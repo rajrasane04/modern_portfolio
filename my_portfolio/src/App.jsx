fa85277b5542836f19032d78b753d670daa91808
Raj Rasane <rasaneraj04@gmail.com>
2024-09-08 19:38:56 +0530

    set up REACT environment

---
+++ my_portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { Route , Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navbar/>} />
+      </Routes>
+    </>
+    
+  )
+}
+
+export default App
